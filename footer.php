5c803076fbb974358fd5dcf9af5f9c6c5d50ba43
microsheep <twmicrosheep@gmail.com>
2015-07-24 01:58:20 +0800

    add icon and resource page

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer class="footer">
     <h5 style="text-align: center; color: white"><i class="fa fa-copyright"></i> 交大學聯會 2015 Created by : Microsheep</h5>
     <h6 style="text-align: center; color: white">建議使用Google Chrome或Mozilla Firefox以獲得最佳瀏覽效果</h6>
+    <h6 style="text-align: center; color: white">Icons from Freepik and font-awesome</h6>
 </footer>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
--- resource.php
+++ resource.php
@@ -1,9 +1,30 @@
 <?php
     include_once("./header.php");
 ?>
-
-
-
+<style>
+    body{
+        background-image: url(./asset/photo/home_top_bg.jpg);
+        background-size: cover;
+    }
+</style>
+<div class="jumbotron" style="background-color:transparent">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-5">
+            <div class="container text-center">
+                <h1>如何申請D2郵箱？</h1><br>
+                <p><b>剛到了交大D2信箱是每個人都需要的必需品</b></p>
+                <a class="btn btn-success btn-lg" href="https://d2.nctu.edu.tw/apply/" role="button">辦D2帳號 &raquo; </a>
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3 text-center">
+            <br>
+            <img src="./asset/icon/send15.png" style="width: 100%;max-width: 300px"> 
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
 
 <?php
     include_once("footer.php");
