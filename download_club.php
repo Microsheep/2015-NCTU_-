528689f0c6fb84188f1dc91eb4002ee83cff5395
microsheep <twmicrosheep@gmail.com>
2015-07-23 21:29:49 +0800

    add club page

---
+++ download_club.php
@@ -0,0 +1,68 @@
+<?php
+    include_once("./header.php");
+?>
+<br>
+<div class="row">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-10">
+        <table class="table table-hover table-bordered table-condensed">
+            <thead>
+                <tr class="warning">
+                    <td>編號</td>
+                    <td>社團名稱</td>
+                    <td>線上預覽</td>
+                    <td>Download</td>
+                    <td>社團 FB</td>
+                    <td>社團網頁</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                for ($i = 0; $i < $club_count; $i++) {
+                    if($i%2==0){
+                        echo "<tr class=\"success\">";
+                    }
+                    else{
+                        echo "<tr>";
+                    }    
+                    echo "<td>#" . ($i+1) . "</td>";
+                    echo "<td>" . $club_name[$i] . "</td>";
+                    if ($club_pdf[$i]!="0"){
+                        echo "<td><a href=./pdf_view.php?id=" . $club_ref[$i] . ">";
+                        echo "<span class=\"glyphicon glyphicon-search\" aria-hidden=\"true\">\t預覽</span>";
+                        echo "</a></td>";
+                        echo "<td><a href=./asset/info/" . $club_ref[$i] . ".pdf>";
+                        echo "<span class=\"glyphicon glyphicon-download-alt\" aria-hidden=\"true\">\t下載</span>";
+                        echo "</a></td>";
+                    }
+                    else{
+                        echo "<td></td><td></td>";
+                    }
+                    if ($club_fb[$i]!=""){
+                        echo "<td><a href=" . $club_fb[$i] . ">";
+                        echo "<span class=\"glyphicon glyphicon-education\" aria-hidden=\"true\">\t友會FB</a></td>";
+                    }
+                    else{
+                        echo "<td></td>";
+                    } 
+                    if ($club_web[$i]!=""){
+                        echo "<td><a href=" . $club_web[$i] . ">";
+                        echo "<span class=\"glyphicon glyphicon-home\" aria-hidden=\"true\">\t友會網頁</span></a></td>";
+                    }
+                    else{
+                        echo "<td></td>";
+                    }    
+                    echo "</tr>";
+                }
+            ?>
+            </tbody>
+        </table>
+        <h4 style="text-align:center">(註：該欄空白代表該社團並未提供該項資料)</h4>
+    </div>
+    <div class="col-md-1">
+    </div>
+</div>
+<?php
+    include_once("footer.php");
+?>
--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <link href="https://fonts.googleapis.com/earlyaccess/cwtexhei.css" rel="stylesheet">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <style>
-    body{font-family: 'cwTeXHei', serif}
+    body{font-family: 'cwTeXHei', serif;}
     .table thead>tr>td{vertical-align: middle}
     .table tbody>tr>td{vertical-align: middle}
     .table thead>tr{text-align: center}
@@ -28,6 +28,7 @@
 <?php
     global $school_count,$school_name,$school_ref,$school_pdf,$school_fb,$school_web,$school_newcomer;
     global $region_count,$region_name,$region_ref,$region_pdf,$region_fb,$region_web,$region_newcomer;
+    global $club_count,$club_name,$club_ref,$club_pdf,$club_fb,$club_web;
     global $page_count,$page_name;
     $school_count=20;
     $school_name=["電機資訊學士班","奈米科學及工程學士學位學程","電子工程學系","電機工程學系","光電工程學系","資訊工程學系","材料科學與工程學系","機械工程學系","土木工程學系","電子物理學系","應用化學系","應用數學系","生物科技學系","資訊管理與財務金融學系","管理科學系","運輸與物流管理學系","工業工程與管理學系","外國語文學系","傳播與科技學系","人文社會學系"];
@@ -43,6 +44,13 @@
     $region_fb=["","","","","","","","","","","","","","","","","","","",""];
     $region_web=["","","","","","","","","","","","","","","","","","","",""];
     $region_newcomer=["","","","","","","","","","","","","","","","","","","",""];
+    $club_count=1;
+    $club_name=["我是一個社團","","","","","","","","","","","","","","","","","","",""];
+    $club_ref=["","","","","","","","","","","","","","","","","","","",""];
+    $club_pdf=["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"];
+    $club_fb=["","","","","","","","","","","","","","","","","","","",""];
+    $club_web=["","","","","","","","","","","","","","","","","","","",""];
+    $club_newcomer=["","","","","","","","","","","","","","","","","","","",""];
     $page_count=2;
     $page_name=["歡迎來到交大","新生地圖"];
 ?>
@@ -87,6 +95,7 @@
                                 <li><a href="./download_region.php">友會</a></li>
                             </ul>
                         </li>
+                        <li class="active"><a href="./download_club.php">社團資訊</a></li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li class="active"><a href=http://aadm.nctu.edu.tw/registra/freshman/freshman_a.html>交大新生入學指引網</a></li>
