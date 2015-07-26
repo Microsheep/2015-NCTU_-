b4aee32a08764239433f5d2a96544e6d5c4ed2a7
microsheep <twmicrosheep@gmail.com>
2015-07-26 11:56:42 +0800

    resource finalized

--- resource.php
+++ resource.php
@@ -32,16 +32,90 @@
         <div class="col-md-2"></div>
         <div class="col-md-3 text-center">
             <br>
+            <img src="./asset/photo/configure.png" style="width: 100%;max-width: 300px"> 
+        </div>
+        <div class="col-md-5">
+            <br>
+            <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
+                <h1>新生的資料準備</h1><br>
+                <p><b>剛進交大 有許多資料需要繳交</b></p>
+                <p><b>列出一張清單，提早把資料找齊吧！</b></p>
+                <p><div class="btn-group">
+                    <a class="btn btn-primary btn-lg" href="http://aadm.nctu.edu.tw/registra/freshman/freshman_c.html" role="button">入學前資料準備 &raquo; </a>
+                    <a class="btn btn-danger btn-lg" href="http://aadm.nctu.edu.tw/registra/freshman/freshman_g.html" role="button">學雜費、就學貸款與獎學金 &raquo; </a>
+                    <a class="btn btn-success btn-lg" href="http://aadm.nctu.edu.tw/registra/freshman/freshman_d.html" role="button">註冊事項 &raquo; </a>
+                </div></p>
+            </div> 
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
+<div class="jumbotron" style="margin-bottom: 0px">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3 text-center">
+            <br>
+            <img src="./asset/photo/skeleton.png" style="width: 100%;max-width: 300px"> 
+        </div>
+        <div class="col-md-5">
+            <br>
+            <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
+                <h1>新生輔導、健康檢查</h1><br>
+                <p><b>為期4天的新生輔導都在做些甚麼呢？</b></p>
+                <p><b>聽演講、逛交大、唱第一次校歌</b></p>
+                <p><b>檢康檢查、院系輔導、社團博覽會</b></p>
+                <p><b>還有好多有趣的事情呢！</b></p>
+                <p><div class="btn-group">
+                    <a class="btn btn-info btn-lg" href="http://aadm.nctu.edu.tw/registra/freshman/freshman_b.html" role="button">新生輔導 &raquo; </a>
+                    <a class="btn btn-warning btn-lg" href="http://aadm.nctu.edu.tw/registra/freshman/freshman_f.html" role="button">健康檢查 &raquo; </a>
+                </div></p>
+            </div> 
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
+<div class="jumbotron" style="margin-bottom: 0px">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3 text-center">
+            <br>
+            <img src="./asset/photo/soldier1.png" style="width: 100%;max-width: 300px"> 
+        </div>
+        <div class="col-md-5">
+            <br>
+            <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
+                <h1>兵役</h1><br>
+                <p><b>看我們的隊伍雄壯威武，聽我們的歌聲響徹雲霄...</b></p>
+                <p><b>每個男人都會有兵役的問題需要處理</b></p>
+                <p><b>注意兵役的事項，免得到時候在學期中收到兵單，還得跑一趟</b></p>
+                <p><div class="btn-group">
+                    <a class="btn btn-success btn-lg" href="http://aadm.nctu.edu.tw/registra/freshman/freshman_m.html" role="button">兵役事項說明 &raquo; </a>
+                    <a class="btn btn-primary btn-lg" href="http://a9388001.blogspot.com/p/httpsregist.html" role="button">緩徵儘召申請說明 &raquo; </a>
+                </div></p>
+            </div> 
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
+<div class="jumbotron" style="margin-bottom: 0px">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3 text-center">
+            <br>
             <img src="./asset/photo/re_home.png" style="width: 100%;max-width: 300px"> 
         </div>
         <div class="col-md-5">
             <br>
             <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
                 <h1>新生宿舍申請</h1><br>
-                <p><b></b></p>
-                <p><b></b></p>
-                <p><b></b></p>
-                <p><a class="btn btn-success btn-lg" href="http://dormapply2.adm.nctu.edu.tw/freshman/index.asp" role="button">新生宿舍申請 &raquo; </a></p>
+                <p><b>交大新生宿舍最特別的莫過於同系的住在一起</b></p>
+                <p><b>晚上吃消夜、聊天、討論功課十分的方便</b></p>
+                <p><b>別忘記在8/13中午12點以前申請宿舍唷，免得造成遺憾</b></p>
+                <p><div class="btn-group">
+                    <a class="btn btn-success btn-lg" href="http://dormapply2.adm.nctu.edu.tw/freshman/index.asp" role="button">新生宿舍申請 &raquo; </a>
+                    <a class="btn btn-danger btn-lg" href="http://aadm.nctu.edu.tw/registra/freshman/freshman_h.html" role="button">新生宿舍說明 &raquo; </a>
+                    <a class="btn btn-primary btn-lg" href="http://housing.adm.nctu.edu.tw/" role="button">住宿服務組 &raquo; </a>
+                </div></p>
             </div> 
         </div>
         <div class="col-md-2"></div>
@@ -58,9 +132,9 @@
             <br>
             <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
                 <h1>申請D2帳號</h1><br>
-                <p><b></b></p>
-                <p><b></b></p>
-                <p><b></b></p>
+                <p><b>D2 信箱帳號是交大網路服務的必備帳號</b></p>
+                <p><b>舉凡使用宿舍網路，連交大無線網路都需要他</b></p>
+                <p><b>同時它也是交大給學生的專用信箱 ( @nctu.edu.tw )</b></p>
                 <p><a class="btn btn-success btn-lg" href="https://d2.nctu.edu.tw/apply/" role="button">D2 信箱申請 &raquo; </a></p>
             </div> 
         </div>
@@ -78,9 +152,9 @@
             <br>
             <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
                 <h1>如何連接學校網路</h1><br>
-                <p><b></b></p>
-                <p><b></b></p>
-                <p><b></b></p>
+                <p><b>光世代是什麼? 100M? 300M?</b></p>
+                <p><b>來到交大，宿舍網路擁有 1G (1000M) 的網路速度</b></p>
+                <p><b>快照下面的方法連上宿網，體驗網路速度上限是網路線速度上限的感覺</b></p>
                 <p><div class="btn-group">
                     <a class="btn btn-info btn-lg" href="http://www.it.nctu.edu.tw/itweb/modules/catalog_1/custom_NavigationLink/view.php?l_id=3" role="button">宿舍網路連線方法 &raquo; </a>
                     <a class="btn btn-warning btn-lg" href="http://www.it.nctu.edu.tw/itweb/modules/catalog_1/custom_NavigationLink/view.php?l_id=2" role="button">校內無線網路 &raquo; </a>
@@ -101,10 +175,13 @@
             <br>
             <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
                 <h1>選課系統</h1><br>
-                <p><b></b></p>
-                <p><b></b></p>
-                <p><b></b></p>
+                <p><b>什麼時候要選課阿？</b></p>
+                <p><b>大一必修有哪些阿？ 要選哪些課阿？</b></p>
+                <p><b>哪個老師教的比較好？</b></p>
+                <p><b>NCTU+ 系統可以協助大家選課與排課，歡迎大家多多使用</b></p>
+                <p>註：NCTU+ 系統為選課輔助系統，正式選課請至選課系統網站進行選課</p>
                 <p><div class="btn-group">
+                    <a class="btn btn-warning btn-lg" href="http://aadm.nctu.edu.tw/registra/freshman/freshman_e.html" role="button">選課說明 &raquo; </a>
                     <a class="btn btn-success btn-lg" href="http://timetable.nctu.edu.tw/" role="button">課程時間表 &raquo; </a>
                     <a class="btn btn-danger btn-lg" href="https://course.nctu.edu.tw/" role="button">選課系統 &raquo; </a>
                     <a class="btn btn-primary btn-lg" href="http://plus.nctu.edu.tw/" role="button">NCTU+ 選課輔助系統 &raquo; </a>
@@ -125,9 +202,10 @@
             <br>
             <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
                 <h1>英文學分抵免</h1><br>
-                <p><b></b></p>
-                <p><b></b></p>
-                <p><b></b></p>
+                <p><b>大一英文可以不用修嗎?</b></p>
+                <p><b>第二外語要怎麼選呢?</b></p>
+                <p><b>為什麼有些人大二還要修英文</b></p>
+                <p><b>看看外語課程修習辦法來了解這一切吧！</b></p>
                 <p><div class="btn-group">
                     <a class="btn btn-warning btn-lg" href="http://ltrc.nctu.edu.tw/news_o.php?id=214" role="button">外語課程修習辦法 &raquo; </a>
                     <a class="btn btn-danger btn-lg" href="http://ltrc.nctu.edu.tw/news_o.php?id=121" role="button">常見問題 Q&A &raquo; </a>
@@ -148,9 +226,9 @@
             <br>
             <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
                 <h1>新竹的交通工具</h1><br>
-                <p><b></b></p>
-                <p><b></b></p>
-                <p><b></b></p>
+                <p><b>1路、2路 傻傻分不清楚</b></p>
+                <p><b>小紅巴又是什麼?</b></p>
+                <p><b>快點開下面的連結了解新竹的交通工具吧！</b></p>
                 <p><div class="btn-group">
                     <a class="btn btn-primary btn-lg" href="./info_bus.php" role="button">公車 &raquo; </a>
                     <a class="btn btn-info btn-lg" href="./info_longbus.php" role="button">客運 &raquo; </a>
@@ -172,9 +250,9 @@
             <br>
             <div class="container text-center" style="background-color: rgba(153,255,51,0.3); -moz-border-radius: 20px; border-radius: 20px;">
                 <h1>交大卡和學聯會憑證福利</h1><br>
-                <p><b></b></p>
-                <p><b></b></p>
-                <p><b></b></p>
+                <p><b>交大學聯會與士林卡聯合推出</b></p>
+                <p><b>超過 800 間店家的交大特約系統</b></p>
+                <p><b>限量 2000 張，提供給有學聯會憑證的交大同學</b></p>
                 <p><div class="btn-group">
                     <a class="btn btn-danger btn-lg" href="./direct.php#9" role="button">交大卡 &raquo; </a>
                     <a class="btn btn-info btn-lg" href="./direct.php#14" role="button">學聯會憑證 &raquo; </a>
@@ -184,6 +262,9 @@
         <div class="col-md-2"></div>
     </div>
 </div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="./asset/bs-back-to-top.js"></script>
 
 <?php
     include_once("footer.php");
